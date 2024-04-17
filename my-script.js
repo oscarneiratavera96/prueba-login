989ff67635dd9fe841f570c332ced6c1cbc6dddd
Oscar <oscar.neira@tinet.cl>
2024-04-17 16:16:34 -0500

    ADD: Se agregan estilos

---
+++ my-script.js
@@ -0,0 +1,5 @@
+// Selecciona el formulario por su ID
+const formulario = document.getElementById("localAccountForm");
+
+// Agrega el atributo 'novalidate' al formulario
+formulario.setAttribute("novalidate", "true");
